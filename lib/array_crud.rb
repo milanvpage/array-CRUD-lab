4d9fe510b72f552aac40b9110e5f0482a637879a
Milan <milanvpage@gmail.com>
2021-03-03 13:49:05 -0800

    commit tree c1237c2ac1b8e5f182eeb3da408f7945b0b51b97

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,35 +1,46 @@
 def create_an_empty_array
-  
+    []
 end
 
 def create_an_array
-  
+  ["pink","blue","purple","green"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+    array = ["array","element"]
+    array.push "arrays!"
 end
 
 def add_element_to_start_of_array(array, element)
+    array = ["array","element"]
+    array.unshift "wow"
   
 end
 
 def remove_element_from_end_of_array(array)
-  
+    array = ["array","arrays!"]
+    array = array.pop  
 end
 
 def remove_element_from_start_of_array(array)
+    array = ["wow","array"]
+    array = array.shift
   
 end
 
 def retrieve_element_from_index(array, index_number)
+    array = ["am","index_number"]
+    array[0]
   
 end
 
 def retrieve_first_element_from_array(array)
+    array = ["wow","array"]
+    array[0]
   
 end
 
 def retrieve_last_element_from_array(array)
-  
+    array = ["array","arrays!"]
+    array[-1]
 end
